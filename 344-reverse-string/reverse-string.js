3039a650637f5814f47cd6015998d96885b80ebe
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-14 16:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 62 MB (28.50%) - LeetSync

---
+++ 344-reverse-string/reverse-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    let len = s.length;
+    let halfLen = Math.floor(len / 2);
+
+    for (let i = 0; i < halfLen; i++) {
+        //swap
+        let temp = s[i];
+        s[i] = s[len - 1 - i];
+        s[len - 1 - i] = temp;
+    }
+};
+
+// in this my first should be swapped by the last then we will move the i
